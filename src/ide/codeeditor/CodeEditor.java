4d1eb4e2ee7f998ed71209ca5d93ccdbdd623606
Junior <juniorbecari@gmail.com>
2021-07-13 10:04:43 -0300

    Removida a coloração de tags no CSS, pois já colore automaticamente como variáveis

--- src/ide/codeeditor/CodeEditor.java
+++ src/ide/codeeditor/CodeEditor.java
@@ -1458,50 +1458,12 @@ public class CodeEditor extends IDEComponent {
 					"output", "p", "param", "picture", "pre", "progress", "q", "rp", "rt", "ruby", "s", "samp", "script",
 					"section", "select", "small", "source", "span", "strike", "strong", "style", "sup", "svg", "table",
 					"tbody", "td", "template", "textarea", "tfoot", "th", "thead", "time", "title", "tr", "track", "tt",
-					"u", "ul", "var", "video", "wbr", "important", "from", "to"
+					"u", "ul", "var", "video", "wbr"
 			};
 			
-			String[] props = { "align-content", "align-items", "all", "animation", "animation-direction",
-					"animation-duration", "animation-fill-mode", "animation-iteration-count", "animation-name",
-					"animation-play-state", "animation-timing-function", "backface-visibility", "background",
-					"background-attachment", "background-blend-mode", "background-clip", "background-color",
-					"background-image", "background-origin", "background-position", "background-repeat",
-					"background-size", "border", "border-bottom", "border-bottom-color", "border-bottom-left-radius",
-					"border-bottom-right-radius", "border-bottom-style", "border-bottom-width", "border-collapse",
-					"border-color", "border-image", "border-image-outset", "border-image-repeat", "border-image-slice",
-					"border-image-source", "border-image-width", "border-radius", "border-right", "border-right-color",
-					"border-right-style", "border-right-width", "border-spacing", "border-style", "border-top",
-					"border-top-color", "border-top-left-radius", "border-top-right-radius", "border-top-style",
-					"border-top-width", "border-width", "bottom", "box-decoration-break", "box-shadow", "box-sizing",
-					"break-after", "break-before", "break-inside", "caption-side", "caret-color", "@charset", "clear",
-					"clip", "color", "column-count", "column-fill", "column-gap", "column-rule", "column-rule-color",
-					"column-rule-style", "column-rule-width", "column-span", "column-width", "columns", "content",
-					"counter-increment", "counter-reset", "cursor", "direction", "display", "empty-cells", "filter",
-					"flex", "flex-basis", "flex-direction", "flex-flow", "flex-grow", "flex-shrink", "flex-wrap",
-					"float", "font", "@font-face", "font-family", "font-feature-settings", "@font-feature-values",
-					"font-kerning", "font-language-override", "font-size", "font-size-adjust", "font-stretch",
-					"font-style", "font-synthesis", "font-variant", "font-variant-alternates", "font-variant-caps",
-					"font-variant-east-asian", "font-variant-ligatures", "font-variant-numeric", "font-variant-position",
-					"font-weight", "gap", "grid", "grid-area", "grid-auto-columns", "grid-auto-flow", "grid-auto-rows",
-					"grid-column", "grid-column-end", "grid-column-gap", "grid-column-start", "grid-template",
-					"grid-template-areas", "grid-template-columns", "grid-template-rows", "hanging-ponctuation",
-					"height", "hyphens", "image-rendering", "@import", "isolation", "justify-content", "@keyframes",
-					"left", "letter-spacing", "line-break", "line-height", "list-style", "list-style-image",
-					"list-style-position", "list-style-type", "margin", "margin-bottom", "margin-left",
-					"margin-right", "margin-top", "mask", "mask-type", "max-height", "max-width", "@media",
-					"min-height", "min-width", "mix-blend-mode", "object-fit", "object-position", "opacity",
-					"order", "orphans", "outline", "outline-color", "outline-offset", "outline-style",
-					"outline-width", "overflow", "overflow-wrap", "overflow-x", "overflow-y", "padding",
-					"padding-bottom", "padding-left", "padding-right", "padding-top", "page-break-after",
-					"page-break-before", "page-break-inside", "perspective", "perspective-origin", "pointer-events",
-					"position", "quotes", "resize", "right", "row-gap", "scroll-behavior", "tab-size", "table-layout",
-					"text-align", "text-align-last", "text-combine-upright", "text-decoration", "text-decoration-color",
-					"text-decoration-line", "text-decoration-style", "text-indent", "text-justify", "text-orientation",
-					"text-overflow", "text-shadow", "text-transform", "text-underline-position", "top", "transform",
-					"transform-origin", "transform-style", "transition", "transition-delay", "transition-duration",
-					"transition-property", "transition-timing-function", "unicode-bidi", "user-select", "vertical-align",
-					"visibility", "white-space", "widows", "width", "word-break", "word-spacing", "word-wrap",
-					"writing-mode", "z-index", "screen", "and" };
+			// As outras são coloridas automaticamente como variável
+			String[] props = { "@charset", "@font-face", "@font-feature-values", "@import", "@keyframes", "@media",
+					"screen", "and", "important", "from", "to", "bottom", "top", "right", "left" };
 			
 			String[] units = { "px", "em", "rem", "cm", "mm", "in", "pt", "pc", "ex", "ch", "vw", "vh", "vmin", "vmax" };
 			
