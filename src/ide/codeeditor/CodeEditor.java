93e39b105bf5c333eae5f61313126451eff97ad3
Antônio Carlos <juniorbecari@gmail.com>
2021-08-30 16:03:09 -0300

    Agora colore debug em JS e TS.

--- src/ide/codeeditor/CodeEditor.java
+++ src/ide/codeeditor/CodeEditor.java
@@ -260,7 +260,7 @@ public class CodeEditor extends IDEComponent {
 			"native", "new", "null", "package", "private", "protected", "public", "return", "short", "static",
 			"super", "switch", "synchronized", "this", "throw", "throws", "transient", "true", "try", "typeof",
 			"var", "void", "volatile", "while", "with", "yield", "undefined", "of", "async", "window", "document",
-			"console", "as", "from", "navigator", "constructor" };
+			"console", "as", "from", "navigator", "constructor", "debug" };
 	
 	public static final String[] cssTags = { "a", "abbr", "acronym", "address", "applet", "area", "article",
 			"aside", "audio", "b", "base", "basefont", "bdi", "bdo", "big", "blockquote", "body", "br", "button",
@@ -607,7 +607,7 @@ public class CodeEditor extends IDEComponent {
 			"throws", "eval", "extends", "null", "final", "true", "try", "implements", "private", "const",
 			"import", "string", "for", "interface", "delete", "switch", "public", "of", "await", "class",
 			"console", "false", "volatile", "any", "int", "instanceof", "super", "with", "async",
-			"boolean", "short", "arguments", "window", "as", "from", "navigator", "constructor" };
+			"boolean", "short", "arguments", "window", "as", "from", "navigator", "constructor", "debug" };
 	
 	public static final String[] ktKeys = { "as", "as?", "break", "class", "continue", "do", "else", "false", "for", "fun",
 			"if", "in", "!in", "interface", "is", "!is", "null", "object", "package", "return", "super",
