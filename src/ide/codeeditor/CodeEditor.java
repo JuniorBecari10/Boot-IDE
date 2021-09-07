a9902e1f49144bfe70feb5dae6fcc5124dedcec1
Antônio Carlos <juniorbecari@gmail.com>
2021-09-07 13:40:03 -0300

    Agora colore self em Lua

--- src/ide/codeeditor/CodeEditor.java
+++ src/ide/codeeditor/CodeEditor.java
@@ -406,7 +406,7 @@ public class CodeEditor extends IDEComponent {
 	public static final String[] luaKeys = { "and", "break", "do", "else", "elseif", "end",
 			"false", "for", "function", "if", "in", "local", "nil",
 			"not", "or", "repeat", "return", "then", "true", "until", "while",
-			"os", "io", "math", "string", "require", "table", "debug" };
+			"os", "io", "math", "string", "require", "table", "debug", "self" };
 	
 	public static final String[] zigKeys = { "align", "allowzero", "and", "anyframe", "anytype", "asm", "async", "await",
 			"break", "catch", "comptime", "const", "continue", "defer", "else", "enum", "errdefer",
