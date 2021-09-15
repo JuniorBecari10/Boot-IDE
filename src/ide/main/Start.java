11b7c8ae46716ae45097d863ad1b992a244ffd6b
Antônio Carlos <juniorbecari@gmail.com>
2021-09-15 10:15:30 -0300

    No método main, alterado a ordem das coisas por lá

--- src/ide/main/Start.java
+++ src/ide/main/Start.java
@@ -2,8 +2,8 @@ package ide.main;
 
 public class Start {
     public static void main(String[] args) {
+    	Main.args = args;
+    	
     	new Main().start();
-        
-        Main.args = args;
     }
 }
