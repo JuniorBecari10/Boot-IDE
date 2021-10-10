48071cf18c96ed58c79b30d01bbbe1646f7a99a6
Antônio Carlos <juniorbecari@gmail.com>
2021-10-10 13:01:10 -0300

    Não colore mais center em CSS, por motivos de bug

--- src/ide/codeeditor/CodeEditor.java
+++ src/ide/codeeditor/CodeEditor.java
@@ -272,7 +272,7 @@ public class CodeEditor extends IDEComponent {
 	
 	public static final String[] cssTags = { "a", "abbr", "acronym", "address", "applet", "area", "article",
 			"aside", "audio", "b", "base", "basefont", "bdi", "bdo", "big", "blockquote", "body", "br", "button",
-			"canvas", "caption", "center", "cite", "code", "col", "colgroup", "data", "datalist", "dd", "del",
+			"canvas", "caption", "cite", "code", "col", "colgroup", "data", "datalist", "dd", "del",
 			"details", "dfn", "dialog", "dir", "div", "dl", "dt", "em", "embed", "fieldset", "figcaption", "figure",
 			"font", "footer", "form", "frame", "frameset", "h1", "h2", "h3", "h4", "h5", "h6", "head", "header",
 			"hr", "html", "i", "iframe", "img", "input", "ins", "kbd", "label", "legend", "li", "link", "main",
