d03b857fc04b9fb7c209a69a515c706cfc01db14
Antônio Carlos <juniorbecari@gmail.com>
2022-04-15 10:42:20 -0300

    Release

--- src/ide/main/Main.java
+++ src/ide/main/Main.java
@@ -66,7 +66,7 @@ public class Main implements Runnable, Tickable {
     public static final String LOG_FILE_NAME = "Exception.log";
     
     public static final String PROGRAM_NAME = "Boot IDE";
-    public static final String VERSION = "Beta 2 v4.4";
+    public static final String VERSION = "Beta 3 v4.4";
     
     public static BufferedImage UNKNOWN_FILE_ICON = null;
     
