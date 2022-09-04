7b2294ac76073acad8fd21d3afda9c6940dd3bd9
Antônio Carlos <juniorbecari@gmail.com>
2022-09-04 11:56:32 -0300

    Desenhados ícones das operações com branches

---
+++ src/ide/explorercomponents/ExecuteButtonIcon.java
@@ -0,0 +1,47 @@
+package ide.explorercomponents;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import ide.components.CommandTerminal;
+import ide.components.IDEComponent;
+import ide.components.RenameFile;
+import ide.components.SetFileName;
+import ide.explorer.Explorer;
+import ide.input.KeyInput;
+import ide.input.MouseInput;
+import ide.util.Colors;
+
+public class ExecuteButtonIcon extends IDEComponent {
+
+	protected Execute execute;
+	protected String caption;
+	
+	public ExecuteButtonIcon(int x, int y, int width, int height, BufferedImage sprite, Execute execute, String caption) {
+		super(x, y, width, height, sprite);
+		
+		this.execute = execute;
+		this.caption = caption;
+	}
+	
+	public void tick() {
+		if (leftClicked()) {
+			KeyInput.updateKeys();
+			
+			execute.execute();
+		}
+	}
+	
+	public void render(Graphics g) {
+		if (hovered()) {
+			g.setColor(Colors.explorerLight);
+			g.fillRect(x, y, width, height);
+		}
+		
+		super.render(g);
+		
+		if (hovered() && !(SetFileName.added || RenameFile.added || CommandTerminal.active)) {
+			Explorer.renderDescriptionText(caption, MouseInput.getMouseX() - 27, MouseInput.getMouseY() + 27, g);
+		}
+	}
+}
