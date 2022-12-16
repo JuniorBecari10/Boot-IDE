3fe3a3ad78f93fcc5cae8ddef9e421247aa9ebba
Antônio Carlos <juniorbecari@gmail.com>
2022-12-16 18:16:52 -0300

    O FileChooser agora aparece o título

--- src/ide/components/FileChooser.java
+++ src/ide/components/FileChooser.java
@@ -1,8 +1,12 @@
 package ide.components;
 
+import java.awt.Graphics;
 import java.io.File;
 import java.util.ArrayList;
 
+import ide.codeeditor.CodeEditor;
+import ide.fonts.Fonts;
+import ide.fonts.IDEFont;
 import ide.main.Main;
 
 public class FileChooser extends CustomMessageBox {
@@ -28,5 +32,12 @@ public class FileChooser extends CustomMessageBox {
 		FileChooser f = new FileChooser(folder, onlyDirs, title);
 		
 		IDEComponent.toAdd.add(f);
+	}
+	
+	public void render(Graphics g) {
+		super.render(g);
+		
+		Fonts.drawString(title, (Main.screen.frame.getWidth() / 2) - (title.length() * (CodeEditor.DEFAULT_FONT_SIZE - 4)) / 2, y + 10, new IDEFont(Fonts.lightGrayNormal, 16), g);
+		g.drawLine(x + 15, y + 40, x + width - 15, y + 40);
 	}
 }
